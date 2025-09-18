675cb57613b6830fbecdd02e8c22fddccdd6eaa6
NatanCDalmarco <natanuy@gmail.com>
2025-09-18 15:15:52 -0300

    feat: Atualizando Model Animal

--- src/main/java/api_rest/zoologico/Models/Animal.java
+++ src/main/java/api_rest/zoologico/Models/Animal.java
@@ -1,22 +1,29 @@
 package api_rest.zoologico.Models;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Animal {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
- private String nome;
+    private String nome;
+    private String especie;
+    private int idade;
 
-    public Long getId() {
-        return id;
-    }
+    @ManyToOne
+    @JoinColumn(name = "habitat_id", nullable = false)
+    private Habitat habitat;
 
-public String getNome() {
-        return nome;
-    }
-
+    @ManyToOne
+    @JoinColumn(name = "cuidador_id", nullable = false)
+    private Cuidador cuidador;
 }
